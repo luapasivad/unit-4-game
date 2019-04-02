9b67afc5d31bba20f271d69623f15e9f3edc1434
Paul Davis <pauldavis@Pauls-MacBook-Pro.local>
2019-04-02 18:38:58 -0400

    barebones - but working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -27,88 +27,308 @@
 - as long as the user stays alive, the game will loop until there are no opponents left.
 */
 
-//character grab  
-var jack = $('#jack')
-var davy = $('#davy')
-var barb = $('#barb')
-var swann = $('#swann')
-//char select variable
-var charSelectVar = 0
-//char variables
+//PUT ANIMATION INTO SEPERATE FUNCTIONS
+
+
+//character grab
+var jon = $('#jon')
+var brienne = $('#brienne')
+var daenerys = $('#daenerys')
+var khal = $('#khal')
+//enemy grab
+var ramsey = $('#ramsey')
+var cersei = $('#cersei')
+var melisandre = $('#melisandre')
+var nightKing = $('#nightKing')
+
+var fightBtn = $('#fightBtn')
+//char stats in obj.
 var character = {  
     
-    charD:{
+    charB:{
         hp: 120,
-        att: 6,
-        cAtt: 10
+        att: 8
     },
 
     charJ:{
         hp: 100,
         att: 11,
-        cAtt: 6
     },
 
-    charB:{
+    charD:{
         hp: 110,
-        att: 9,
-        cAtt: 9
+        att: 9
     },
 
-    charS:{
+    charK:{
         hp: 80,
-        att: 15,
-        cAtt: 3
+        att: 15
     }
 
 } 
-//char select
-jack.click(function(){
-    charSelectVar = 1
-    charSelect()
-})
-davy.click(function(){
-    charSelectVar = 2
-    charSelect()
-})
-barb.click(function(){
-    charSelectVar = 3
-    charSelect()
-})
-swann.click(function(){
-    charSelectVar = 4
-    charSelect()
-})
-
-function charSelect(){
-    switch (charSelectVar) {
-        case 1:
-            console.log("jack");
-            leaveAnimate()
-            break;
-        case 2:
-            console.log("davy");
-            jack.remove();
-            barb.remove();
-            swann.remove();
-            break;
-        case 3:
-            console.log("barb");
-            jack.remove();
-            davy.remove();
-            swann.remove();
-            break;
-        case 4:
-            console.log("swann");
-            davy.remove();
-            barb.remove();
-            jack.remove();
-            break;
+//enemy stats in obj
+var enemies = {
+    enemRamsey:{
+        hp: 80,
+        cAtt: 4 
+    },
+    enemCersei:{
+        hp: 100,
+        cAtt: 6  
+    },
+    enemMelisandre:{
+        hp: 120,
+        cAtt: 8
+    },
+    enemNight:{
+        hp: 150,
+        cAtt: 10,  
     }
 }
-//maybe work on animate later
-function leaveAnimate() {
-    swann.animate({height: 'toggle'})
-    davy.animate({height: 'toggle'})
-    barb.animate({height: 'toggle'})
+
+//animation presets
+function fade() {
+    ramsey.fadeOut(1)
+    cersei.fadeOut(1)
+    melisandre.fadeOut(1)
+    nightKing.fadeOut(1)
+    fightBtn.fadeOut(1)
+} 
+function charFadeOut() {
+    khal.fadeOut(400)
+    brienne.fadeOut(400)
+    daenerys.fadeOut(400)
+    jon.fadeOut(400)
+}
+function charFadeIn() {
+    khal.delay(600).fadeIn(800)
+    brienne.delay(600).fadeIn(800)
+    daenerys.delay(600).fadeIn(800)
+    jon.delay(600).fadeIn(800)
+}
+function enemyFadeOut() {
+    ramsey.fadeOut(400)
+    cersei.fadeOut(400)
+    melisandre.fadeOut(400)
+    nightKing.fadeOut(400)
+}
+function enemyFadeIn() {
+    console.log('boo')
+    ramsey.delay(600).fadeIn(800)
+    cersei.delay(600).fadeIn(800)
+    melisandre.delay(600).fadeIn(800)
+    nightKing.delay(600).fadeIn(800)
+}
+//user character name
+var userChar = ""
+//stats pertaining to character
+var charStats = ""
+//amount att increases by each attacl
+var attackIncrementor = 0
+fade()
+jon.click(function(){
+    userChar = "Jon Snow"
+    charStats = character.charJ;
+    attackIncrementor = character.charJ.att;
+    selectCharAnimate()
+})
+brienne.click(function(){
+    userChar = "Brienne of Tarth"
+    charStats = character.charB;
+    attackIncrementor = character.charD.att;
+    selectCharAnimate()
+})
+daenerys.click(function(){
+    userChar = "Daenerys Targaryan"
+    charStats = character.charD;
+    attackIncrementor = character.charB.att;
+    selectCharAnimate()
+})
+khal.click(function(){
+    userChar = "Khal Drogo"
+    charStats = character.charK;
+    attackIncrementor = character.charS.att;
+    selectCharAnimate()
+})
+//wrapper where the selected character
+//will go
+var hero = $('#charWrapper')  
+//where the characters health is
+var statBar = $('#statBar')
+//preset animation for enemy fadeIn
+//char select animation
+function selectCharAnimate() {
+    console.log('boo')
+    //fadeOut after selection
+    charFadeOut()
+    //created divs for selected character
+    //and health added to stat bar
+    hero.append('<div id="userChar"></div>')
+    statBar.append('<div id="hpUser"></div>')
+    //preset for animation followed by fadeIn
+    $('#userChar').fadeOut(1)
+    $('#userChar').delay(600).fadeIn(800)
+    //adding name of character and
+    //health
+    $('#userChar').html(userChar)
+    $('#hpUser').html(charStats.hp)
+    //reveal enemy select
+    enemyFadeIn()
+}
+
+//this is the same as the user select
+var enemyChar = ""
+var enemyStats = ""
+var enemySelected = ""
+ramsey.click(function(){
+    enemChar = "Ramsey Bolton"
+    enemySelected = "ramsey";
+    enemyStats = enemies.enemRamsey;
+    selectEnemyAnimate()
+})
+cersei.click(function(){
+    enemChar = "Cersei Lanister"
+    enemySelected = "cersei";
+    enemyStats = enemies.enemCersei;
+    selectEnemyAnimate()
+})
+melisandre.click(function(){
+    enemChar = "Melisandre"
+    enemySelected = "melisandre";
+    enemyStats = enemies.enemMelisandre;
+    selectEnemyAnimate()
+})
+nightKing.click(function(){
+    enemChar = "The Night King"
+    enemySelected = "nightKing";
+    enemyStats = enemies.enemNight;
+    selectEnemyAnimate()
+})
+
+//this is the same as the user
+var enemy = $('#enemyWrapper')  
+var enemyStatBar = $('#enemyStatBar')
+
+function selectEnemyAnimate() {
+    enemyFadeOut();
+    enemy.append('<div class="opponent" id="opponent"></div>')
+    enemyStatBar.append('<div id="hpEnemy"></div>')
+
+    $('#opponent').fadeOut(1)
+    $('#opponent').delay(600).fadeIn(800)
+
+    $('#opponent').html(enemChar)
+    $('#hpEnemy').html(enemyStats.hp)
+
+    fightBtn.delay(600).fadeIn(800)
+    }
+
+//fight function
+var enemiesDefeated = 0
+function fight() {
+    if (charStats.hp > 0 && enemyStats.hp >0) {
+        //health calculation
+        enemyStats.hp =  enemyStats.hp - charStats.att
+        charStats.hp = charStats.hp - enemyStats.cAtt;
+        charStats.att = attackIncrementor + charStats.att;
+        
+        
+        console.log(charStats.hp)
+        console.log(enemyStats.hp)
+        //test update
+        $('#hpEnemy').html(enemyStats.hp)
+        $('#hpUser').html(charStats.hp)
+        //win/lose check
+        if (charStats.hp <=0) {
+            console.log("YOU LOSE")
+            gameOverReset()
+        } else if (enemyStats.hp <=0) {
+            $('#'+ enemySelected).addClass('hidden')
+            enemyDefeat()    
+        }
+    }
+}
+
+function enemyDefeat() {
+    $('#opponent').delay(600).fadeOut(800, function() {
+        $('#opponent').remove()
+    })
+    fightBtn.delay(600).fadeOut(800)
+    $('#hpEnemy').html("")
+    enemiesDefeated++
+        if (enemiesDefeated == 4) {
+            console.log("YOU WIN THE GAME")
+            gameOverReset()
+        } else if (enemiesDefeated < 4) {
+            enemyFadeIn();
+        }
+}
+
+function gameOverReset() {
+    character = {  
+        charD:{
+            hp: 1,
+            att: 1,
+            cAtt: 10
+        },
+    
+        charJ:{
+            hp: 100,
+            att: 11,
+            cAtt: 6
+        },
+
+        charB:{
+            hp: 110,
+            att: 9,
+            cAtt: 9
+        },
+    
+        charS:{
+            hp: 80,
+            att: 15,
+            cAtt: 3
+        }
+    }  
+    enemies = {
+        enemRamsey:{
+            hp: 110,
+            cAtt: 12 
+        },
+        enemCersei:{
+            hp: 90,
+            cAtt: 9  
+        },
+        enemMelisandre:{
+            hp: 70,
+            cAtt: 4
+        },
+        enemNight:{
+            hp: 150,
+            cAtt: 16,  
+        }
+    }
+    userChar = ""
+    charStats = ""
+    attackIncrementor = 0
+    enemyChar = ""
+    enemyStats = ""
+    enemiesDefeated = 0
+    $('#hpEnemy').html("")
+    $('#hpUser').html("")
+    hero.fadeOut(1000, function() {
+        hero.empty();
+        hero.fadeIn(1)
+    })
+    enemy.fadeOut(1000, function() {
+        enemy.empty();
+        enemy.fadeIn(1)
+    })
+    fightBtn.fadeOut(1000);
+    charFadeIn()
+    ramsey.removeClass('hidden')
+    cersei.removeClass('hidden')
+    melisandre.removeClass('hidden')
+    nightKing.removeClass('hidden')
+
 }
